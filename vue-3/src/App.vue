21a4a0b35226be7afecfbf67db0f380aa97da154
Laptop <prononny@gmail.com>
2023-11-07 17:51:07 +0700

    no message

---
+++ vue-3/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <BNav>
+    <router-link to="/"><BNavItem><i-mdi-home/> Home</BNavItem></router-link>
+    <router-link to="/brand"><BNavItem><i-mdi-TshirtCrew/> Brand</BNavItem></router-link>
+    <router-link to="/order"><BNavItem><i-mdi-cart/> Order</BNavItem></router-link>
+  </BNav>
+  
+  <router-view></router-view>
+</template>
+
+<style>
+svg {
+  color: darkgoldenrod;
+  height: .8rem;
+  margin-right: 3px;
+}
+
+.nav {
+  background-color: #06192c;
+  justify-content: space-evenly;
+  align-content: center;
+  height: 4rem;
+  margin-bottom: 2rem !important;
+}
+
+.nav a {
+  font-size: small;
+  color: gainsboro;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+}
+
+.nav a.router-link-exact-active {
+  color: white;
+}
+</style>
---
+++ vue-3/src/views/GallaryView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home">
+        <h1>
+          All <span style="color: darkgoldenrod; margin-reight: 5px">
+            COLLECTION</span>
+        </h1>
+        <displayProduct name="" :product="product"></displayProduct>
+      </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import displayProduct from "../components/displayProduct.vue";
+import products from "../assets/products.json"
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      product: products,
+    };
+  },
+  components: {
+    displayProduct,
+  },
+};
+</script>
